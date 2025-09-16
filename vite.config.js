59e2b8e7a7ee8ad54d646947b7b51b07cbb4c8b9
[Adrin Neville] <[adrinneville11@gmail.com]>
2025-09-16 10:24:33 +0530

    5th commit for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-  define: {
-    __FASTAPI_URL__: JSON.stringify(process.env.VITE_FASTAPI_URL),
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    define: {
+      __FASTAPI_URL__: JSON.stringify(env.VITE_FASTAPI_URL),
+    },
+  };
 });
